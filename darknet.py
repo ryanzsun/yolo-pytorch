e0c8deed67fdaeb7cdb1927a3c91f40e69f62055
zhensun <zhen.sun@westwell-lab.com>
2019-02-01 15:13:19 +0800

    darknet

--- darknet.py
+++ darknet.py
@@ -1,11 +1,25 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import numpy as np
 
 class ResidualBlock(nn.modules):
-    def __init__(self, in_channels, out_cahnnels, stride = 1, downsample = None):
+    def __init__(self, in_channels, out_channels, stride = 1, downsample = None):
         super(ResidualBlock, self).__init__()
-        self.
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size = 1,
+                               stride = 1, padding = 1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size = 3,)
+                               stride = 1, padding = 1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+    def forward(self, x):
+        residual = x
+        x = F.leaky_relu(self.bn1(self.conv1(x)))
+        x = F.leaky_relu(self.bn2(self.conv2(x)))
+        x = x + residual
+        return x
+
 
 class Darknet(nn.modules):
     def __init__(self, in_channels, num_classes):
@@ -20,4 +34,4 @@ class Darknet(nn.modules):
         self.layer4 = self._make_layer(residual_block, , layers[3])
         self.layer5 = self._make_layer(residual_block, , layers[4])
 
-        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        
